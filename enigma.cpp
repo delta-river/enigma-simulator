9b9c990055ae7e11eb19f755b4f858da07abbed8
delta-river <taigarana@LAPTOP-Q2DC233A.localdomain>
2020-01-28 19:57:58 +0900

    prepared classes

--- enigma.cpp
+++ enigma.cpp
@@ -2,8 +2,38 @@
 using namespace std;
 
 class Rotor{
-		
-}
+		int count;
+		string alph;
+	public:
+		Rotor();
+		char scramble(char);
+};
+
+class Reflector{
+		string alph;
+	public:
+		Reflector();
+		char reflect(char);
+};
+
+class Plugboard{
+		string alph;
+	public:
+		Plugboard();
+		char scramble(char);
+};
+
+class Enigma{
+		Plugboard plugboard;
+		Rotor right_rotor;
+		Rotor middle_rotor;
+		Rotor left_rotor;
+		Reflector reflector;
+	public:
+		Enigma();
+		void set();
+		char encode(char);
+};
 
 int main(){
 		return 0;
